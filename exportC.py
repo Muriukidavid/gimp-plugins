5a99fe4e49b0504d6cba435873647f4f50448961
David Muriuki <karfes@gmail.com>
2015-08-12 15:59:59 +0300

    cleaned up exportC.py removed the showWin function that was moved to new_icon.py removed some debug terminal output lines removed commented unused imports and codes

--- exportC.py
+++ exportC.py
@@ -1,20 +1,12 @@
 #!/usr/bin/python
 
 from gimpfu import *
-#from array import array
 
 def ord3(data): 
     r=data[0]
     g=data[1]
     b=data[2]
     return "{%(i)d,%(j)d,%(k)d}" % {"i":ord(r), "j":ord(g), "k":ord(b)}
-#TODO: Implement GUI for creating a new graphic of a given LCD size   
-def showWin(width, height):
-    img = gimp.Image(width,height,RGB)
-    lyr = gimp.Layer(img, 'layer1', width, height, RGB, 100, NORMAL_MODE) 
-    img.add_layer(lyr)
-    gimp.Display(img)
-    gimp.displays_flush()
     
 def plugin_main(timg, tdrawable,filename):
     pr =tdrawable.get_pixel_rgn(0, 0, timg.width,timg.height, False, False);
@@ -27,7 +19,6 @@ def plugin_main(timg, tdrawable,filename):
         while (i<timg.width-1):
             output += ord3(pr[i,j]) + ","
             i+=1
-        #print "j is: %(jj)d" % {"jj":j}
         output += ord3(pr[i,j]) #the last column, no comma
         output += ",\n" #end of each row
         j+=1
@@ -37,13 +28,10 @@ def plugin_main(timg, tdrawable,filename):
         i+=1
     output += ord3(pr[i,j]) #the last column, no comma
     output += "\n};"
-    print output #show output on terminal
            
     savefile = open(filename,"w")
-    #savefile.write("{%(a)d %(b)d %(c)d}" % {"a":ord(x[0]), "b":ord(x[1]), "c":ord(x[2])})
     savefile.write(output)
     savefile.close()
-    #showWin(30,10)
     
     
 register(
