a881da7e82226cd45c9806b9e76a20c39d8acce9
David Muriuki <karfes@gmail.com>
2015-08-13 16:33:51 +0300

    Restructured the code Added options to export in 4 different modes, with only 2 modes implemented: 16-bit(5-6-5) and 24-bit(8-8-8 in a struct of bytes) Added error messages for the unimplemented modes and no mode selected Cleaned up the code a bit more :)

--- exportC.py
+++ exportC.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 
-from gimpfu import *
+from gimpfu import register, PF_IMAGE, PF_DRAWABLE, PF_STRING, PF_OPTION, main,gimp
 
 def ord3(data): 
     r=data[0]
@@ -14,26 +14,16 @@ def packColor(data):
     b=ord(data[2])
     return str((r << 11) & 0xf800 | (g << 5) & 0x07c0 | (b & 0x003f))
     
-def plugin_main(timg, tdrawable,filename,colormode):
+def plugin_main(timg, tdrawable,iconname,filename,colormode):
     pr =tdrawable.get_pixel_rgn(0, 0, timg.width,timg.height, False, False);
     i=0
     j=0
     size = timg.width*timg.height #LCD size
-    output = "typedef struct{ \nunsigned char \nr,\ng,\nb;\n}RGB;\n\nRGB image[" + "%(size)d" % {"size":size} + "]={\n" 
+    output = "/*\nC code Exported from Gimp\n\nicon size in pixels: width="+str(timg.width)+", height:"+str(timg.height)+"\n*/\n\n" 
     if colormode==0:
-        while (j<timg.height-1):
-            while (i<timg.width-1):
-                output += ord3(pr[i,j]) + ","
-                i+=1
-            output += ord3(pr[i,j]) #the last column, no comma
-            output += ",\n" #end of each row
-            j+=1
-            i=0
-        while (i<timg.height-1): #the last row, no comma
-            output += ord3(pr[i,j]) + ","
-            i+=1
-        output += ord3(pr[i,j]) #the last column, no comma
-    elif colormode==2:
+        gimp.message("12-bit mode not yet implemented")
+    elif colormode==1:
+        output += "uint16_t "+iconname+"[" + "%(size)d" % {"size":size} + "]={\n" 
         while (j<timg.height-1):
             while (i<timg.width-1):
                 output += packColor(pr[i,j]) + ","
@@ -46,12 +36,33 @@ def plugin_main(timg, tdrawable,filename,colormode):
             output += packColor(pr[i,j]) + ","
             i+=1
         output += packColor(pr[i,j]) #the last column, no comma
-    output += "\n};"
-           
-    savefile = open(filename,"w")
-    savefile.write(output)
-    savefile.close()
-    
+        output += "\n};"
+        savefile = open(filename,"w")
+        savefile.write(output)
+        savefile.close() 
+    elif colormode==2:
+        gimp.message("18-bit mode not yet implemented")
+    elif colormode==3:
+        output += "typedef struct{ \nunsigned char \nr,\ng,\nb;\n}RGB;\n\nRGB "+iconname+"[" + "%(size)d" % {"size":size} + "]={\n" 
+        while (j<timg.height-1):
+            while (i<timg.width-1):
+                output += ord3(pr[i,j]) + ","
+                i+=1
+            output += ord3(pr[i,j]) #the last column, no comma
+            output += ",\n" #end of each row
+            j+=1
+            i=0
+        while (i<timg.height-1): #the last row, no comma
+            output += ord3(pr[i,j]) + ","
+            i+=1
+        output += ord3(pr[i,j]) #the last column, no comma
+        output += "\n};"
+        savefile = open(filename,"w")
+        savefile.write(output)
+        savefile.close() 
+    else:
+        gimp.message("Error: No color mode selected")    
+  
     
 register(
         "export-C-code",
@@ -65,8 +76,9 @@ register(
         [
          (PF_IMAGE, "timg", "Input image", None),
          (PF_DRAWABLE, "tdrawable", "Input drawable", None),
+         (PF_STRING, "iconname", "Icon variable name","icon"),
          (PF_STRING,"filename","file to save code to","untitled.c"),
-         (PF_OPTION,"colormode","Color Mode",2,("RGB","12-bit","16-bit", "24-bit"))
+         (PF_OPTION,"colormode","RGB Color Mode",4,("12-bit(4-4-4): not implemented","16-bit(5-6-5)", "18-bit(6-6-6): not implemented","24-bit(8-8-8)"))
         ],
         [],
         plugin_main, menu="<Image>/File/Export/")
